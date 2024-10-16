81017e277e56d4ecf86964e66f42cec6e6bc08fa
Hitesh Jadhav <jadhavhitesh207@gmail.com>
2024-10-17 02:43:54 +0530

    update ui

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,3 @@
-// const Login = (props) =>{
-//     return(
-//         <div className="login-container">
-//             <h1 className="welcome-message">Welcome to decentralized voting application</h1>
-//             <button className="login-button" onClick={props.connectWallet}>Login Metamask</button>
-//         </div>
-//     )
-// }
-
-
-// export default Login;
-
-
 const Login = (props) => {
     return (
         <div className="login-container">
